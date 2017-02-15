e43106189ceee33bed00d6ffcb08d88d9253f102
john <john@voilamediagroup.com>
2017-02-15 15:43:14 -0600

    adding in live db for testing

--- js/settings.js
+++ js/settings.js
@@ -1,4 +1,4 @@
 ﻿var MickmanAppLogin = MickmanAppLogin || {};
 MickmanAppLogin.Settings = MickmanAppLogin.Settings || {};
-MickmanAppLogin.Settings.signInUrl = "http://localhost:8888/UserLogin/login.php"; 
+MickmanAppLogin.Settings.signInUrl = "http://cpanel-2101-20.datarealm.com/~farasisc/test-mick/login.php";
 MickmanAppLogin.Settings.sessionTimeoutInMSec = 31500000; //364 days 14 hours
