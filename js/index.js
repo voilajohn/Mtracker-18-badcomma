1308c2a5e5311395b80d4bf2b34b77d049334fcb
john <john@voilamediagroup.com>
2017-03-03 09:34:42 -0600

    added carousel

--- js/index.js
+++ js/index.js
@@ -92,6 +92,7 @@ $(document).delegate("#page-signin", "pagebeforecreate", function () {
 $(document).delegate("#page-main-menu", "pagebeforecreate", function () {
 	app.catalogController.init();
     app.catalogController.getSavedData();
+    
 	$('.product-display').slick({
     	centerMode: true,
 		centerPadding: '60px',
@@ -117,6 +118,7 @@ $(document).delegate("#page-main-menu", "pagebeforecreate", function () {
 	    }
 		]  	
 	});
+	
 });
 
 //Cart Page is Loaded
