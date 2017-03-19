b1c7a0a35d71ae0bd924604c479c714b925dee46
john <john@voilamediagroup.com>
2017-03-19 06:11:03 -0500

    moving from iframe to just the panzoom plugin

--- js/index.js
+++ js/index.js
@@ -61,54 +61,6 @@ function checkGroup(){ //find the group name and the user saved.
 	console.log("gcheck");
 }
 
-function scale( width, height, padding, border ) {
-    var scrWidth = $( window ).width() - 30,
-        scrHeight = $( window ).height() - 30,
-        ifrPadding = 2 * padding,
-        ifrBorder = 2 * border,
-        ifrWidth = width + ifrPadding + ifrBorder,
-        ifrHeight = height + ifrPadding + ifrBorder,
-        h, w;
-
-    if ( ifrWidth < scrWidth && ifrHeight < scrHeight ) {
-        w = ifrWidth;
-        h = ifrHeight;
-    } else if ( ( ifrWidth / scrWidth ) > ( ifrHeight / scrHeight ) ) {
-        w = scrWidth;
-        h = ( scrWidth / ifrWidth ) * ifrHeight;
-    } else {
-        h = scrHeight;
-        w = ( scrHeight / ifrHeight ) * ifrWidth;
-    }
-
-    return {
-        'width': w - ( ifrPadding + ifrBorder ),
-        'height': h - ( ifrPadding + ifrBorder )
-    };
-};
-$( document ).on( "pageinit", function() {
-    $( "#larger iframe" )
-        .attr( "width", 0 )
-        .attr( "height", 0 );
-		  
-    $( "#larger" ).on({
-        popupbeforeposition: function() {
-            var size = scale( 497, 298, 15, 1 ),
-                w = size.width,
-                h = size.height;
-
-            $( "#larger iframe" )
-                .attr( "width", w )
-                .attr( "height", h );
-        },
-        popupafterclose: function() {
-            $( "#larger iframe" )
-                .attr( "width", 0 )
-                .attr( "height", 0 );    
-        }
-    });
-});
-
 $(document).on("pagecontainerbeforeshow", function (event, ui) {
     if (typeof ui.toPage == "object") {
 	    
