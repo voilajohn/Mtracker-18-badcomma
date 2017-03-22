af97bcb7f81b4c2cb1abea82c7263d47cd436077
john <john@voilamediagroup.com>
2017-03-22 15:05:30 -0500

    fixed typo

--- js/order-controller.js
+++ js/order-controller.js
@@ -141,8 +141,6 @@ $(document).on('click', '.fullOrder', function(){ //Cart + button
 	app.orderController.addorderDatatoPopup($(this).data('orderid'));
 });
 
-mickman-session	{"userProfileModel":"John","sessionId":"3049385","expirationDate":"2017-03-22T22:55:56.587Z","keepSignedIn":true}
-
 //print page
 $(document).on('click', '.printOrders', function(){
 	//first lets organize the content of the orders
