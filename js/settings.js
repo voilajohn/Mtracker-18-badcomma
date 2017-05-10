8cd1842d952dcdeee25ba7ba6ff90050c8e24d12
john <john@voilamediagroup.com>
2017-05-10 14:50:18 -0500

    form fixes

--- js/settings.js
+++ js/settings.js
@@ -4,6 +4,11 @@ MickmanAppLogin.Settings.signInUrl = "http://cpanel-2101-20.datarealm.com/~faras
 MickmanAppLogin.Settings.tokenUrl = "http://cpanel-2101-20.datarealm.com/~farasisc/test-mick/token.php";
 MickmanAppLogin.Settings.syncDataUrl = "http://cpanel-2101-20.datarealm.com/~farasisc/test-mick/sync.php";
 //MickmanAppLogin.Settings.signInUrl = "http://mickman.com/login.php";
+https://online.mickman.com/app/login.php
+
+//MickmanAppLogin.Settings.signInUrl = "https://online.mickman.com/app/login.php";
+//MickmanAppLogin.Settings.tokenUrl = "https://online.mickman.com/app/token.php";
+//MickmanAppLogin.Settings.syncDataUrl = "https://online.mickman.com/app/sync.php";
 MickmanAppLogin.Settings.sessionTimeoutInMSec = 31500000; //364 days 14 hours
 
 //database storage
